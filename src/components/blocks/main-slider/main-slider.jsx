e23abf2df00091abaa91baeb499cab06e8f1f002
Alexey A <allvxm4@gmail.com>
2025-03-23 12:33:21 +0300

    main page st2, adaptive

--- src/components/blocks/main-slider/main-slider.jsx
+++ src/components/blocks/main-slider/main-slider.jsx
@@ -1,10 +1,11 @@
 'use client'
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectFade, Autoplay } from 'swiper/modules';
+import { EffectFade, Autoplay, Pagination } from 'swiper/modules';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/effect-fade';
+import 'swiper/css/pagination';
 
 import {slides} from "@/data/main-slider";
 import MainSliderCard from "@/components/blocks/main-slider/main-slider-card";
@@ -13,10 +14,11 @@ const MainSlider = () => {
   return (
     <Swiper
       className="main-slider"
-      modules={[EffectFade, Autoplay]}
+      modules={[EffectFade, Autoplay, Pagination]}
       effect="fade"
       spaceBetween={0}
       loop={true}
+      pagination={true}
       autoplay={{
         enabled: false,
         delay: 10000,
