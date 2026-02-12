6e08f8cc91e09c4385c4317fe8e90a213c2891da
Alexey A <allvxm4@gmail.com>
2026-02-12 19:08:07 +0300

    page products

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,9 @@
 export const API_URL = `https://vagclub21.ru/api`;
 // export const API_URL = `http://localhost:5000/api`;
-// export const CMS_URL = `https://cms.vagclub21.ru`;
-export const CMS_URL = `http://localhost:3000`;
-// export const API_CMS_URL = `https://cms.vagclub21.ru/api`;
-export const API_CMS_URL = `http://localhost:3000/api`;
+export const CMS_URL = `https://cms.vagclub21.ru`;
+// export const CMS_URL = `http://localhost:3000`;
+export const API_CMS_URL = `https://cms.vagclub21.ru/api`;
+// export const API_CMS_URL = `http://localhost:3000/api`;
 
 export const BACKEND_SOCIAL_AUTH_URL = `${API_URL}/auth`
 export const TG_AUTH_REDIRECT_URL = `${BACKEND_SOCIAL_AUTH_URL}/telegram/redirect`
