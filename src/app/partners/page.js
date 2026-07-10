82d83039413223a4ef5beb01a903f103d485d6ac
Alexey A <allvxm4@gmail.com>
2026-07-10 23:14:15 +0300

    partners filters fix

--- src/app/partners/page.js
+++ src/app/partners/page.js
@@ -1,5 +1,7 @@
 import {PUBLIC_PAGES} from "@/config/pages/public.config";
 import PartnersContent from "@/components/pages/partners/partners-content";
+import {Suspense} from "react";
+import Loading from "@/app/loading";
 
 export const metadata = {
   title: PUBLIC_PAGES.PARTNERS.SEO_TITLE,
@@ -7,7 +9,9 @@ export const metadata = {
 };
 
 const Page = () => {
-  return <PartnersContent />;
+  return <Suspense fallback={<Loading/>}>
+    <PartnersContent />
+  </Suspense>;
 };
 
 export default Page;
