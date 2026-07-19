00b1d654c3b570e330fd29aec870bc07f0a1eb73
Alexey A <allvxm4@gmail.com>
2026-07-19 22:29:13 +0300

    optimize st2

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 import MainPage from "@/components/pages/_main";
 
 export default async function Home() {
-  'use cache'
   return <MainPage/>;
 }
 
--- src/app/policy/page.js
+++ src/app/policy/page.js
@@ -7,7 +7,6 @@ export const metadata = {
 };
 
 const Page = async () => {
-  'use cache'
   return <PolicyContent/>;
 };
 
